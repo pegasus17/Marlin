34a7dd06a5b95cbd8704f0a70a2c499010d6be08
Roland Gärtner <roland@gardeners.at>
2020-10-09 17:06:32 +0200

    added MACHINE_UUID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -135,7 +135,7 @@
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "s4320224-ca51-4a72-b8ce-0dffd205bf5c"
 
 // @section extruder
 
